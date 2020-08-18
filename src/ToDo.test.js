c8659b6fda8cbfbb50cfd33b1d03d0d4093ac2de
Rohan Bhatia <rohanbhatia96@outlook.com>
2020-08-18 18:08:51 +0530

    Simulate user interactions in our tests

--- src/ToDo.test.js
+++ src/ToDo.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { TodoApp } from "./ToDo";
 
 test("TODO heading present (check from ToDo Component)", () => {
@@ -10,4 +10,15 @@ test("TODO heading present (check from ToDo Component)", () => {
 test("Check label for todo app", () => {
   render(<TodoApp />);
   expect(screen.getByLabelText("What needs to be done?")).not.toBeNull();
+});
+
+test("Check button click", () => {
+  const { getByText, getByLabelText } = render(<TodoApp />);
+  const input = getByLabelText("What needs to be done?");
+  fireEvent.change(input, {
+    target: { value: "Rohan Bhatia" },
+  });
+  fireEvent.click(getByText("Add #1"));
+  //no need to wrap around expect as get by text will throw an error anyway if text is not found
+  getByText("Rohan Bhatia");
 });
